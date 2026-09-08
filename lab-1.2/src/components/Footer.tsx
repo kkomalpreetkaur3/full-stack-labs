d3c96359385c76b5ffcd24c2ae0e18cb0abbe275
komal Aulakh <kkomalpreetkaur@academic.rrc.ca>
2026-09-08 16:46:44 -0500

    Add Header and Footer components

---
+++ lab-1.2/src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export function Footer() {
+    const currentYear = new Date().getFullYear()
+    return(
+        <footer>
+            <p>Copyright Pixell River Financial {currentYear}.</p>
+        </footer>
+    )
+}
---
+++ lab-1.2/src/components/Header.tsx
@@ -0,0 +1,16 @@
+export function Header() {
+  return (
+    <header>
+        <img
+            src="https://itsm-ace.ca/images/logo.svg"
+            alt="Pixell River Financial logo"
+        />
+        
+        <div>
+            <h1>Pixell River Employee Directory</h1>
+            <p>Welcome! Explore our team and departments.</p>
+        </div>
+        
+    </header>
+  )
+}
