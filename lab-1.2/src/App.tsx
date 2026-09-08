97a1446397a03dca1c27ab7ede060a80b935be6b
komal Aulakh <kkomalpreetkaur@academic.rrc.ca>
2026-09-08 16:49:18 -0500

    Connect components in App

---
+++ lab-1.2/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
+import { EmployeeDirectory } from './components/EmployeeDirectory'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <EmployeeDirectory/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
